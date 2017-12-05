5036329e2fe5f8e6ec48e1e1dd4af7a83fb0d469
Renata Oliveira <renatabels@gmail.com>
2017-12-04 23:37:27 -0200

    Implementing the game

--- ColorGame.js
+++ ColorGame.js
@@ -1,2 +1,63 @@
-//alert("Connected!");
+var colors = generateRandomColors(6);
 
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //Add initial colors to the squares
+    squares[i].style.background = colors[i];
+    //Add click listeners to squares
+    squares[i].addEventListener("click", function(){
+    //Grab color of clicked square
+        var clickedColor = this.style.background;
+    //Compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+            h1.style.background = clickedColor;
+        } else {
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+function changeColors(color){
+	//Loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//Change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(number) {
+	//make an array
+	var arr = [];
+	//add num random colors to arr
+	for(var i = 0; i < number; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor() {
+	//Pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//Pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//Pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}

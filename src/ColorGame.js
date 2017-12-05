854a7f62c2769b8b3becb2b24bb1d73f0d494b33
Renata Oliveira <renatabels@gmail.com>
2017-12-05 01:08:38 -0200

    Changing h1 color

--- src/ColorGame.js
+++ src/ColorGame.js
@@ -49,7 +49,7 @@ resetButton.addEventListener("click", function() {
 	for(var i = 0; i < squares.length; i++) {
 		squares[i].style.background = colors[i];
 	}
-	h1.style.background = "#232323";
+	h1.style.background = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;

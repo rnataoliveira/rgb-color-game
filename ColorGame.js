a0e5853a80d8e769a8d7bed1ff9aedd0ef28baf4
Renata Oliveira <renatabels@gmail.com>
2017-12-05 00:39:27 -0200

    Changig the number of squares

--- ColorGame.js
+++ ColorGame.js
@@ -1,5 +1,5 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -12,7 +12,8 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function(){
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
-    colors = generateRandomColors(3);
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i = 0; i < squares.length; i++){
@@ -27,7 +28,8 @@ easyBtn.addEventListener("click", function(){
 hardBtn.addEventListener("click", function(){
     hardBtn.classList.add("selected");
     easyBtn.classList.remove("selected");
-    colors = generateRandomColors(6);
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i = 0; i < squares.length; i++){
@@ -38,7 +40,7 @@ hardBtn.addEventListener("click", function(){
 
 resetButton.addEventListener("click", function() {
 	//Generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	//Pick a new random color from array
 	pickedColor = pickColor();
 	//Change colorDisplay to match picked Color
